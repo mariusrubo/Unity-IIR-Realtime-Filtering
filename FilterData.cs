85085d9514006d23bdcaf3e890b79bd2e3b09878
mariusrubo <marius.rubo@gmail.com>
2021-07-28 10:42:26 +0200

    Update FilterData.cs

--- FilterData.cs
+++ FilterData.cs
@@ -1,28 +1,29 @@
-﻿using System.Collections;
+// Written by Marius Rubo
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FilterData : MonoBehaviour {
 
-    IIRFilter Notch50;
-    IIRFilter Notch100;
+    IIRFilter Low26;
+    IIRFilter High1;
 
     float input;
     float input_filtered1;
     float input_filtered2;
 
-    // Use this for initialization
     void Start () {
-        Notch50 = new IIRFilter(250, 50, 5); // sample rate 250Hz, Notch at 50Hz with bandwidth of 5
-        Notch100 = new IIRFilter(250, 100, 5);
+	Low26 = new IIRFilter(500, 26); // 2nd order low pass butterworth 26hz at 500Hz sampling rate
+	High1 = new IIRFilter(0.991153595101663f, -1.98230719020333f, 0.991153595101663f, 1.98222892979253f, -0.982385450614125f); // 2nd order high pass butterworth 1Hz at 500 Hz sampling rate
     }
 	
-	// Update is called once per frame
-	void Update () {
+
+   void Update () {
 
         // input = SomeDataStream; // continuously stream your data in here. Need not be in Update(), as this is limited to 60Hz. Streaming data into Unity is not part of this script. 
-        input_filtered1 = filter(input, Notch50);
-        input_filtered2 = filter(input_filtered1, Notch100);
+        input_filtered1 = filter(input, Low26);
+        input_filtered2 = filter(input_filtered1, High1);
     }
 
     public class IIRFilter
@@ -38,6 +39,24 @@ public class FilterData : MonoBehaviour {
         public float y1;
         public float y2;
 
+	// two parameters indicate a 2nd order Butterworth low-pass filter
+        // equation obtained here: https://www.codeproject.com/Tips/1092012/A-Butterworth-Filter-in-Csharp
+        public IIRFilter(float samplingrate, float frequency)
+        {
+            const float pi = 3.14159265358979f;
+            float wc = Mathf.Tan(frequency * pi / samplingrate);
+            float k1 = 1.414213562f * wc;
+            float k2 = wc * wc;
+            a0 = k2 / (1 + k1 + k2);
+            a1 = 2 * a0;
+            a2 = a0;
+            float k3 = a1 / k2;
+            b1 = -2 * a0 + k3;
+            b2 = 1 - (2 * a0) - k3;
+
+            x1 = x2 = y1 = y2 = 0;
+        }
+	    
         // three parameters indicates a notch filter
         // equation obtained here http://dspguide.com/ch19/3.htm
         public IIRFilter(float samplingrate, float frequency, float Bandwidth)
@@ -56,27 +75,25 @@ public class FilterData : MonoBehaviour {
             x1 = x2 = y1 = y2 = 0;
         }
 
-        // two parameters indicates a 2nd order Butterworth low-pass filter
-        // equation obtained here: https://www.codeproject.com/Tips/1092012/A-Butterworth-Filter-in-Csharp
-        public IIRFilter(float samplingrate, float frequency)
-        {
-            const float pi = 3.14159265358979f;
-            float wc = Mathf.Tan(frequency * pi / samplingrate);
-            float k1 = 1.414213562f * wc;
-            float k2 = wc * wc;
-            a0 = k2 / (1 + k1 + k2);
-            a1 = 2 * a0;
-            a2 = a0;
-            float k3 = a1 / k2;
-            b1 = -2 * a0 + k3;
-            b2 = 1 - (2 * a0) - k3;
-
-            x1 = x2 = y1 = y2 = 0;
-        }
-
-        // for Butterworth high-pass filters or other IIR filters, you need to insert parameters manually.
-        // Easy way to find these parameters is using R's "signal" package butter function, and convert parameters like this: 
-        // a0 = $b[0]; a1 = $b[1]; a2 = $b[2]; b1 = -$a[1]; b2 = -$a[2]
+	/*
+	Five parameters indicate a generic filter. 
+	This is necessary for Butterworth high-pass filters or other IIR filters (because I could not implement the process of obtaining these parameters in here).  
+        Easy way to find these parameters is using R's "signal" package butter function, and convert parameters like this: 
+	
+	samplingRate <- 500 # in Hz
+	cutoff <- 1 # in Hz
+	
+	order <- 2 # 
+	nyquist <- samplingRate/2
+	W <- cutoff/nyquist
+	bf <- signal::butter(order, W, type = "high")
+        a0<- bf$b[1] / bf$a[1]
+        a1<- bf$b[2] / bf$a[1]
+        a2<- bf$b[3] / bf$a[1]
+        b1<- -bf$a[2] / bf$a[1]
+        b2<- -bf$a[3] / bf$a[1]
+	paste0("new IIRFilter(",a0, "f, " ,a1, "f, ", a2, "f, ", b1, "f, ", b2, "f);") 
+        */
         public IIRFilter(float a0in, float a1in, float a2in, float b1in, float b2in)
         {
             a0 = a0in;
